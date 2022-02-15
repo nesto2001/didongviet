f9a7fd7e6fd0c1dd68a4a3b9a38df94a0d2d731a
Nesto <tanlong6121@gmail.com>
2022-02-15 20:37:39 +0700

    slider

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        touchDrag: true,
+        dotsEach:1
+    });
+  });
