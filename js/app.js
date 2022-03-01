66156aef7432a230ac0e3f3a78af6b1879dd6ee3
Nesto <tanlong6121@gmail.com>
2022-03-01 23:37:38 +0700

    product.html

--- js/app.js
+++ js/app.js
@@ -15,3 +15,19 @@ function cateReadMore() {
     $(".full-text").removeClass('min-full-text');
     return false;
 }
+
+function pickColor(obj){
+    $(obj).closest('div').find(".active").removeClass('active');
+    $(obj).addClass('active');
+    $('input[name="color"]').val($(obj).data("color"));
+}
+
+function pickDeposit(obj){
+    $this=$(obj);
+    $parent=$this.closest('div');
+    $parent.find('.active').removeClass('active');
+    $this.addClass('active');
+
+    $(".register input[name='payment_method']").val($this.data('deposit'));
+    return false;
+}
