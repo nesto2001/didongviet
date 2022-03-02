d60930a51d8396022783bef55c88b177dd34258f
Nesto <tanlong6121@gmail.com>
2022-03-03 06:20:15 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+jQuery(document).ready(function($) {
+    var alterClass = function() {
+      var ww = document.body.clientWidth;
+      if (ww < 600) {
+        $('.test').removeClass('blue');
+      } else if (ww >= 601) {
+        $('.test').addClass('blue');
+      };
+    };
+    $(window).resize(function(){
+      alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
+  });
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items:1,
