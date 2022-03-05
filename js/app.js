35ef171edcd9408f8752ba072bfb1edd6621a00e
Nesto <tanlong6121@gmail.com>
2022-03-06 04:29:23 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,5 @@
-jQuery(document).ready(function($) {
-    var alterClass = function() {
-      var ww = document.body.clientWidth;
-      if (ww < 600) {
-        $('.test').removeClass('blue');
-      } else if (ww >= 601) {
-        $('.test').addClass('blue');
-      };
-    };
-    $(window).resize(function(){
-      alterClass();
-    });
-    //Fire it when the page first loads:
-    alterClass();
-  });
-
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $("#promotion-carousel").owlCarousel({
         items:1,
         dots: true,
         autoplay: true,
@@ -24,7 +8,28 @@ $(document).ready(function(){
         touchDrag: true,
         dotsEach:1
     });
-  });
+
+    $(".bottom-banner").owlCarousel({
+        autoplay: true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        touchDrag: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items: 1,
+                dots: true,
+                dotsEach: 1
+            },
+            600:{
+                items: 2,
+                nav:true,
+                margin: 10,
+                dots: false 
+            }
+        }
+    });
+});
 
 function cateReadMore() {
     $(".cate-read-more").remove();
@@ -47,3 +52,65 @@ function pickDeposit(obj){
     $(".register input[name='payment_method']").val($this.data('deposit'));
     return false;
 }
+
+jQuery(document).ready(function($) {
+  var alterClass = function() {
+    var ww = document.body.clientWidth;
+    var container = $('.container-md.px-0');
+    var desktop = $('.desktop-container');
+    if (ww < 500) {
+      for (i=0; i < container.length ; i++){
+          $(container[i]).removeClass('container-md');
+          $(container[i]).removeClass('px-0');
+      };
+      $('.homepage-menu-cate .menu-cate.d-flex a').unwrap();
+      $('.homepage-menu-cate .menu-cate-content').addClass('desktop-menu-cate');
+      $('.group-featured-apple .box-group-featured .group-left img').attr("src","img/banner/ph_-ki_n-apple-2.jpg");
+    } else if (ww >= 800) {
+        for (i=0; i < desktop.length ; i++){
+            $(desktop[i]).addClass('container-md');
+            $(desktop[i]).addClass('px-0');
+        };
+        $('.homepage-menu-cate .menu-cate-content.desktop-menu-cate a').wrapAll("<div class='menu-cate d-flex'></div>");
+        $('.homepage-menu-cate .menu-cate-content').removeClass('desktop-menu-cate');
+        $('.homepage-content-top .group-featured .homepage-content-wrapper .slider-content .products').addClass('owl-carousel owl-theme');
+        
+        $(".flashsale-carousel").owlCarousel({
+            items:5,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:false,
+            touchDrag: true
+        });
+
+        $(".volunteer-carousel").owlCarousel({
+          items:4,
+          dots: false,
+          autoplay: true,
+          autoplayTimeout:5000,
+          autoplayHoverPause:false,
+          touchDrag: true,
+          margin: 10,
+          loop: true
+        });
+
+        $(".apple-carousel").owlCarousel({
+          items:4,
+          dots: false,
+          autoplay: true,
+          autoplayTimeout:5000,
+          autoplayHoverPause:false,
+          touchDrag: true,
+          margin: 10,
+          loop: true
+        });
+        
+        $('.group-featured-apple .box-group-featured .group-left img').attr("src","img/banner/apple.jpg");
+    };
+  };
+  $(window).resize(function(){
+    alterClass();
+  });
+  alterClass();
+});
