4f0cb7367330aa191819d80044ed73da331762bb
Nesto <tanlong6121@gmail.com>
2022-03-07 20:56:36 +0700

    update

--- js/app.js
+++ js/app.js
@@ -57,24 +57,42 @@ jQuery(document).ready(function($) {
   var alterClass = function() {
     var ww = document.body.clientWidth;
     var container = $('.container-md.px-0');
-    var desktop = $('.desktop-container');
+    var fscarou= $('.flashsale-carousel');
+    var apcarou= $('.apple-carousel');
+    var popcarou= $('.popular-people-slider');
+    var volunteercarou= $('.volunteer-right.influencer-slider');
     if (ww < 500) {
       for (i=0; i < container.length ; i++){
           $(container[i]).removeClass('container-md');
           $(container[i]).removeClass('px-0');
       };
+      $('.desktop-product-view').unwrap();
       $('.homepage-menu-cate .menu-cate.d-flex a').unwrap();
       $('.homepage-menu-cate .menu-cate-content').addClass('desktop-menu-cate');
+      fscarou.trigger('destroy.owl.carousel');
+      apcarou.trigger('destroy.owl.carousel');
+      popcarou.trigger('destroy.owl.carousel');
+      volunteercarou.trigger('destroy.owl.carousel');
+      $('.homepage-content-top .group-featured .homepage-content-wrapper .slider-content .products').removeClass('owl-carousel owl-theme');
+      $('.apple-carousel').removeClass('owl-carousel owl-theme');
+      $('.popular-people-slider').removeClass('owl-carousel owl-theme');
+      $('.volunteer-right.influencer-slider').removeClass('owl-carousel owl-theme');
       $('.group-featured-apple .box-group-featured .group-left img').attr("src","img/banner/ph_-ki_n-apple-2.jpg");
+
     } else if (ww >= 800) {
+        $('.desktop-product-view').wrapAll("<div class='product-specs-info d-flex desktop-container'></div>");
+        var desktop = $('.desktop-container');
         for (i=0; i < desktop.length ; i++){
             $(desktop[i]).addClass('container-md');
             $(desktop[i]).addClass('px-0');
         };
+        $('.apple-carousel').addClass('owl-carousel owl-theme');
+        $('.volunteer-right.influencer-slider').addClass('owl-carousel owl-theme');
+        $('.popular-people-slider').addClass('owl-carousel owl-theme');
+        $('.product-specs-info').removeClass('px-0');
         $('.homepage-menu-cate .menu-cate-content.desktop-menu-cate a').wrapAll("<div class='menu-cate d-flex'></div>");
         $('.homepage-menu-cate .menu-cate-content').removeClass('desktop-menu-cate');
-        $('.homepage-content-top .group-featured .homepage-content-wrapper .slider-content .products').addClass('owl-carousel owl-theme');
-        
+        $('.homepage-content-top .group-featured.desktop-container .homepage-content-wrapper .slider-content .products').addClass('owl-carousel owl-theme');
         $(".flashsale-carousel").owlCarousel({
             items:5,
             dots: false,
@@ -105,7 +123,30 @@ jQuery(document).ready(function($) {
           margin: 10,
           loop: true
         });
-        
+
+        $(".popular-people-slider").owlCarousel({
+            items:4,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout:5000,
+            autoplayHoverPause:false,
+            touchDrag: true,
+            margin: 10,
+            loop: true,
+            autoWidth:true
+        });
+
+        $(".news-slider").owlCarousel({
+            items: 6,
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause:false,
+            touchDrag: true,
+            margin: 10,
+        });
+
+
         $('.group-featured-apple .box-group-featured .group-left img').attr("src","img/banner/apple.jpg");
     };
   };
@@ -114,3 +155,33 @@ jQuery(document).ready(function($) {
   });
   alterClass();
 });
+
+function showImage(obj){
+    $this=$(obj);
+    $('.wait-showroom').css('display','inline-block');
+    var color=$this.data('title');
+    var imgholder=$('.fotorama__active img');
+    var currentActive = $('.opt_attr.active');
+    currentActive.removeClass('active');
+    setTimeout(function(){
+        $('.wait-showroom').css('display','none');
+        if (color === 'Màu xanh'){
+            imgholder.attr('src','img/media/product/samsung-galaxy-s22-128gb-mau-xanh-didongviet.jpeg');
+            $('.fotorama__nav-wrap').attr('style','display: none;');
+        }
+        else if (color === 'Màu trắng'){
+            imgholder.attr('src','img/media/product/samsung-galaxy-s22-128gb-didongviet.jpg');
+            $('.fotorama__nav-wrap').attr('style','display: none;');
+        }
+        else if (color === 'Màu đen'){
+            imgholder.attr('src','img/media/product/samsung-galaxy-s22-128gb-mau-den-didongviet.jpeg');
+            $('.fotorama__nav-wrap').attr('style','display: none;');
+        }
+        else if (color === 'Màu hồng'){
+            imgholder.attr('src','img/media/product/samsung-galaxy-s22-128gb-mau-hong-didongviet.jpeg');
+            $('.fotorama__nav-wrap').attr('style','display: none;');
+        };
+    }, 200);
+    $this.addClass('active');
+    
+}
